c2529e3f05055aea750abe1705ad4e9b4f25b523
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2024-08-11 20:12:11 -0500

    ...

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,4 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend"
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -16,20 +16,22 @@ const schema = a.schema({
       calories: a.integer(),
       category: a.string(),
       metaData: a.json(),
-      available:a.boolean()
-      
+      available: a.boolean(),
     })
-    .authorization((allow) => [allow.guest()]),
-});
+    .authorization((allow) => [allow.guest(), allow.publicApiKey()]),
+})
 
-export type Schema = ClientSchema<typeof schema>;
+export type Schema = ClientSchema<typeof schema>
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
+    defaultAuthorizationMode: "userPool",
+    apiKeyAuthorizationMode: { expiresInDays: 7 },
+
+    //TODO:  add apikey as an authorization mode
   },
-});
+})
 
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
