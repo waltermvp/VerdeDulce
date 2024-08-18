9d9f0a39a72e7822f105e04406d50460b5de57fb
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2024-08-18 17:41:05 -0500

    feat: new UI

---
+++ app/components/AccountItem.tsx
@@ -0,0 +1,46 @@
+import * as React from "react"
+import { StyleProp, TextStyle, View, ViewStyle } from "react-native"
+import { observer } from "mobx-react-lite"
+import { colors, typography } from "app/theme"
+import { Text } from "app/components/Text"
+import { Ionicons } from "@expo/vector-icons"
+
+export interface AccountItemProps {
+  /**
+   * An optional style override useful for padding & margin.
+   */
+  style?: StyleProp<ViewStyle>
+
+  /**
+   * An optional style override useful for padding & margin.
+   */
+  icon: keyof typeof Ionicons.glyphMap
+
+  title: string
+}
+
+/**
+ * Describe your component here
+ */
+export const AccountItem = observer(function AccountItem(props: AccountItemProps) {
+  const { style, title, icon } = props
+  const $styles = [$container, style]
+
+  return (
+    <View style={$styles}>
+      <Ionicons name={icon} />
+      <Text style={$text}>{title}</Text>
+    </View>
+  )
+})
+
+const $container: ViewStyle = {
+  flexDirection: "row",
+  justifyContent: "space-between",
+}
+
+const $text: TextStyle = {
+  fontFamily: typography.primary.normal,
+  fontSize: 14,
+  color: colors.palette.primary500,
+}
