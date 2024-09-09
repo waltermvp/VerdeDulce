f0b50ffebe6e25c0a8864ae888235b7bfcf91731
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2024-09-09 14:23:17 -0500

    feat: QR code linking

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,24 +1,31 @@
 import * as React from "react"
-import { StyleProp, TextStyle, View, ViewStyle } from "react-native"
+import { Pressable, StyleProp, StyleSheet, TextStyle, View, ViewStyle } from "react-native"
 import { observer } from "mobx-react-lite"
 import { colors, spacing, typography } from "app/theme"
 import { Text } from "app/components/Text"
 import { Newsletter } from "./Newsletter"
 import { Bullets } from "./Bullets"
 import { Bullet } from "./Bullet"
+import { QrCodeSvg } from "react-native-qr-svg"
+import { Image } from "expo-image"
+import { imageCDNURL } from "app/utils/linkbuilder"
+const SIZE = 125
+const ICON_SIZE = SIZE / 4.75
+const CONTENT = "https://wa.me/c/593963021783"
 
 export interface FooterProps {
   /**
    * An optional style override useful for padding & margin.
    */
   style?: StyleProp<ViewStyle>
+  onPressQr?: () => void
 }
 
 /**
  * Describe your component here
  */
 export const Footer = observer(function Footer(props: FooterProps) {
-  const { style } = props
+  const { style, onPressQr } = props
   const $styles = [$container, style]
 
   return (
@@ -38,8 +45,9 @@ export const Footer = observer(function Footer(props: FooterProps) {
         style={{
           flex: 1,
           backgroundColor: colors.palette.footerGreen,
-          alignItems: "flex-start",
+          alignItems: "space-between",
           padding: spacing.xl,
+          flexDirection: "row",
 
           // borderTopWidth: 1,
           // borderBottomWidth: 1,
@@ -66,9 +74,34 @@ export const Footer = observer(function Footer(props: FooterProps) {
           ]}
         />
         {/* add copyright information */}
-        <Text style={{ fontStyle: typography.fonts.poppins.light, alignSelf: "flex-end" }}>
-          &copy; 2024 VerdeDulce. All rights reserved.
-        </Text>
+        <View style={{ flex: 1 }}>
+          <Pressable onPress={onPressQr}>
+            <QrCodeSvg
+              style={styles.qr}
+              value={CONTENT}
+              frameSize={SIZE}
+              contentCells={5}
+              content={
+                <Image
+                  style={{ width: ICON_SIZE, height: ICON_SIZE }}
+                  source={{ uri: imageCDNURL("VerdeDulce_logo.png") }}
+                />
+              }
+              backgroundColor={colors.palette.greenFont}
+              dotColor="#ffff"
+              contentStyle={styles.box}
+            />
+            <Text
+              style={{
+                fontStyle: typography.fonts.poppins.light,
+                alignSelf: "flex-end",
+                padding: spacing.md,
+              }}
+            >
+              &copy; 2024 VerdeDulce. All rights reserved.
+            </Text>
+          </Pressable>
+        </View>
       </View>
     </View>
   )
@@ -76,5 +109,30 @@ export const Footer = observer(function Footer(props: FooterProps) {
 
 const $container: ViewStyle = {
   justifyContent: "center",
+  // alignItems: "center",
   // flex: 1,
 }
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  icon: {
+    fontSize: 20,
+  },
+  qr: {
+    padding: 15,
+    alignSelf: "flex-end",
+  },
+})
