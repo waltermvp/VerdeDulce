15baeb54293ae27058fc4bc93e362fa807bbed8d
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2024-09-23 20:44:12 -0500

    ..

---
+++ app/config/config.staging.ts
@@ -0,0 +1,13 @@
+/**
+ * These are configuration settings for the dev environment.
+ *
+ * Do not include API secrets in this file or anywhere in your JS.
+ *
+ * https://reactnative.dev/docs/security#storing-sensitive-info
+ */
+export default {
+  API_URL: "https://api.rss2json.com/v1/",
+  APP_NAME: "dulceverdeDEV",
+  WHATSAPP_CATALOG_URL: "https://wa.me/c/593963021783",
+  MARKETING_STRATEGY: "whatsapp",
+};
